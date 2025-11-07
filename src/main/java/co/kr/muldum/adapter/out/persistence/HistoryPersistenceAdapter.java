10d41dd0d7fd1be48b2f8444a6ab2e4b17bd08bf
randirao <randirao4245@gmail.com>
2025-11-07 11:15:32 +0900

    refactor: Collectors.toList()를 toList()로 개선

--- src/main/java/co/kr/muldum/adapter/out/persistence/HistoryPersistenceAdapter.java
+++ src/main/java/co/kr/muldum/adapter/out/persistence/HistoryPersistenceAdapter.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Component
 public class HistoryPersistenceAdapter implements LoadHistoryPort {
@@ -21,7 +20,7 @@ public class HistoryPersistenceAdapter implements LoadHistoryPort {
     public List<History> findByGeneration(Integer generation) {
         return historyJpaRepository.findByGeneration(generation).stream()
                 .map(HistoryMapper::toDomain)
-                .collect(Collectors.toList());
+                .toList();
     }
 
     @Override
